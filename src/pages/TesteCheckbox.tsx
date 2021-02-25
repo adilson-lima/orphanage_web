41ba46b95c114537f3a687878c1dd45380637d41
Adilson Pereira Lima <adilson.pereira.lima@gmail.com>
2021-02-25 16:25:11 -0300

    Finish NLW

---
+++ src/pages/TesteCheckbox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function TesteCheckbox() {
+    return (
+        <div id='page-teste'>
+            <h1>teste</h1>
+            <h1>teste</h1>
+            <h1>teste</h1>
+            <h1>teste</h1>
+
+            <input type="checkbox"/> <label>Teste</label>
+
+        </div>
+    )
+}
+
+export default TesteCheckbox
